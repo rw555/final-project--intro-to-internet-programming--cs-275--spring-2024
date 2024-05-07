89eb6e7ccd44242a6c5686d1099d68b02e59b709
rw555 <157328874+rw555@users.noreply.github.com>
2024-05-07 08:59:46 -0400

    check input is number

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,5 +3,8 @@ window.onload = () => {
     let input = 0;
     while (input < 2) {
         input = parseInt(window.prompt(`Enter an integer greater than 1`), 10);
+        if (isNaN(input)) {
+            input = 0;
+        }
     }
 };
