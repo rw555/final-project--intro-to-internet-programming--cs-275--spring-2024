a7510acf1658a5d975e29df1cb0a6f1000222e56
rw555 <157328874+rw555@users.noreply.github.com>
2024-05-07 13:22:17 -0400

    fixed promise

--- dev/js/app.js
+++ dev/js/app.js
@@ -10,22 +10,17 @@ window.onload = () => {
         }
         resolve(input);
     });
-    promise
-        .then((input) => {
-            // Create tables to populate
-            for (let tableNumber = 0; tableNumber < 2; tableNumber++) {
-                let tbody = document.querySelectorAll(`tbody`)[tableNumber];
-                for (let iteratorOne = 0; iteratorOne < input; iteratorOne++) {
-                    let tr = document.createElement(`tr`);
-                    for (let iteratorTwo = 0; iteratorTwo < input; iteratorTwo++) {
-                        let td = document.createElement(`td`);
-                        td.innerText = `${((iteratorOne+1) * (iteratorTwo+1))}`;
-                        tr.appendChild(td);
-                    }
-                    tbody.appendChild(tr);
+    let promiseTwo = promise.then((input) => {
+        // Create tables to populate
+        for (let tableNumber = 0; tableNumber < 2; tableNumber++) {
+            let tbody = document.querySelectorAll(`tbody`)[tableNumber];
+            for (let iteratorOne = 0; iteratorOne < input; iteratorOne++) {
+                let tr = document.createElement(`tr`);
+                for (let iteratorTwo = 0; iteratorTwo < input; iteratorTwo++) {
+                    let td = document.createElement(`td`);
+                    td.innerText = `${((iteratorOne+1) * (iteratorTwo+1))}`;
+                    tr.appendChild(td);
                 }
+                tbody.appendChild(tr);
             }
-
-            let range = input * input;
-        });
 };
