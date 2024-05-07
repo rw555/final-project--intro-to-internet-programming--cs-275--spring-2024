3ea5f03aa85a3db8202ecc57e752009cfe3d5466
rw555 <157328874+rw555@users.noreply.github.com>
2024-05-06 21:34:00 -0400

    Prompt for number

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,7 @@
 window.onload = () => {
     // Add your code here
+    let input = 0;
+    while (input < 2) {
+        input = parseInt(window.prompt(`Enter an integer greater than 1`));
+    }
 };
