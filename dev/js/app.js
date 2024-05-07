eeb72930666c21360c7292a1718313ad4b355e68
rw555 <157328874+rw555@users.noreply.github.com>
2024-05-07 08:40:31 -0400

    edited parseInt base

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,6 +2,6 @@ window.onload = () => {
     // Add your code here
     let input = 0;
     while (input < 2) {
-        input = parseInt(window.prompt(`Enter an integer greater than 1`));
+        input = parseInt(window.prompt(`Enter an integer greater than 1`), 10);
     }
 };
