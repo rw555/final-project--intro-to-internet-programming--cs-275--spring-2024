a2d28c75502ceff8f771be1b9e59bd85dbb11bba
rw555 <157328874+rw555@users.noreply.github.com>
2024-05-07 11:53:44 -0400

    promise added

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,10 +1,17 @@
 window.onload = () => {
     // Add your code here
-    let input = 0;
-    while (input < 2) {
-        input = parseInt(window.prompt(`Enter an integer greater than 1`), 10);
-        if (isNaN(input)) {
-            input = 0;
+    let promise = new Promise((resolve) => {
+        let input = 0;
+        while (input < 2) {
+            input = parseInt(window.prompt(`Enter an integer greater than 1`), 10);
+            if (isNaN(input)) {
+                input = 0;
+            }
         }
-    }
+        resolve(input);
+    });
+    promise
+        .then((input) => {
+            let range = input * input;
+        });
 };
